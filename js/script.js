ca30227c23d189a3f1c2e0c2d883e037628d6c61
jahidulkanchan <mdj524156@gmail.com>
2024-08-05 00:25:38 +0600

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ menuBar.addEventListener('click',() =>{
   console.log('menuBar clicked');
   headerMenu.classList.toggle('active');
 });
+
+// ========= Scroll to Top =========
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let pos = document.documentElement.scrollTop;
+
+  let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight); 
+
+  pos > 200? scrollProgress.style.display = "grid": scrollProgress.style.display = "none";
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#08b76b ${scrollValue}%, #f3f3f3 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
 // Owl-carousel
 $('.owl-carousel').owlCarousel({
   loop:true,
