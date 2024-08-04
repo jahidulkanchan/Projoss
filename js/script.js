43d3a72ccb580db89628ecfadb6472815a501f58
jahidulkanchan <mdj524156@gmail.com>
2024-08-04 13:52:39 +0600

    update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ menuBar.addEventListener('click',() =>{
   console.log('menuBar clicked');
   headerMenu.classList.toggle('active');
 });
+// Owl-carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  autoplay: true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true
+      }
+  }
+})
